243c91d7ce8e2bc5690104543e631437493f4153
Tamil <tamil@VAIO>
2017-03-22 01:05:17 +0530

    initial commit

---
+++ src/main/java/com/spring/mvc/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping
+	public ModelAndView sayHello() {
+		return new ModelAndView("HelloPage", "message", "Hello, Im Spring MVC..!");
+	}
+}
